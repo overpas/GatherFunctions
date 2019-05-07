dc9eb13d9ca4cff42371fd8a3f53840fb849703c
overpass <pckeycalculator@gmail.com>
2019-05-07 14:57:45 +0300

    add descriptions and notification of being accepted

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,14 @@
 'use-strict'
 
+// MessageTypes: 1 - a user has enrolled, 2 - you have been accepted;
+// Topics: 	{meeting_id}_pending_users - to be notified when user enrolls;
+// 		{meeting_id}_accepted_{user_id} - to be notified of being accepted
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.sendNotification = functions.firestore.document("Meetings/{meeting_id}/PendingUsers/{pending_id}").onWrite((change, context) => {
+exports.sendNotificationToAdmin = functions.firestore.document("Meetings/{meeting_id}/PendingUsers/{pending_id}").onWrite((change, context) => {
 	const meeting_id = context.params.meeting_id;
 	const pending_id = context.params.pending_id;
 	console.log("Meeting ID = " + meeting_id + ", PendingUser ID = " + pending_id);
@@ -21,8 +25,10 @@ exports.sendNotification = functions.firestore.document("Meetings/{meeting_id}/P
 		console.log("User ID = " + user_id + ", Role = " + role);
 		var message = {
 			data: {
-				id: user_id,
-				role: role
+				user_id: user_id,
+				meeting_id: meeting_id,
+				role: role,
+				type: "1"
 			},
 			topic: topic
 		};
@@ -35,5 +41,33 @@ exports.sendNotification = functions.firestore.document("Meetings/{meeting_id}/P
 		});
 		return;
 	});
+});
+
+exports.sendNotificationToPending = functions.firestore.document("Meetings/{meeting_id}/PendingUsers/{pending_id}").onDelete((snap, context) => {
+	const deleted = snap.data();
+	const meeting_id = context.params.meeting_id;
+	const pending_id = context.params.pending_id;
+	const topic = meeting_id + "_accepted_" + deleted.id;
+	const user_id = deleted.id;
+	const role = deleted.role.toString();
+	console.log("Meeting ID = " + meeting_id + ", PendingUser ID = " + pending_id);
+	console.log("Accepted user ID = " + deleted.id + ", role" + deleted.role);
+	var message = {
+		data: {
+			user_id: user_id,
+			meeting_id: meeting_id,
+			role: role,
+			type: "2"
+		},
+		topic: topic
+	};
+	admin.messaging().send(message).then((response) => {
+		console.log("A data message has been sent ", response);
+		return;
+	})
+	.catch((error) => {
+		console.log("Failed to send message", error);
+	});
+	return "sendNotificationToPending finished";
 });
 
